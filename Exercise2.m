918f1da78ff9aded4e9eddc9195b712080b9a5a4
clnnglc <102605406+clnnglc@users.noreply.github.com>
2022-08-05 22:25:55 +0800

    Add files via upload

---
+++ Exercise2.m
@@ -0,0 +1,13 @@
+f = imread ('fruits.png');
+imfinfo ('fruits.png')
+imshow (f);
+
+pkg load image
+j = imresize (f,0.5);
+imwrite (j,'fruits2.png');
+imfinfo ('fruits2.png')
+figure (2), imshow (j);
+
+hsv_f = rgb2hsv (f);
+imwrite (hsv_f,'fruits3.png');
+figure (3), imshow (hsv_f);
